ea112c4b40ff9608ef77949a9f8c368d3c0fa2cb
Henrysoed <henry.soedibjo@gmail.com>
2024-04-21 18:05:24 +0700

    [REFACTOR] add security for user

--- src/main/java/id/ac/ui/cs/advprog/gametime/auth/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/auth/model/User.java
@@ -1,37 +1,37 @@
 package id.ac.ui.cs.advprog.gametime.auth.model;
 
-import id.ac.ui.cs.advprog.gametime.auth.model.Builder.UserBuilder;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
-    private Integer id;
-
-    @Column(nullable = false)
     @Getter
+    private Integer id;
+    @Column(nullable = false)
     @Setter
     private String username;
-
     @Column(unique = true, length = 255, nullable = false)
     @Getter
     @Setter
     private String email;
-
-    @Column(nullable = false)
-    @Setter
-    private String password;
-
     @Column(nullable = false)
     @Getter
     @Setter
-    private String type;
+    private String password;
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String role;
 
     @Column(nullable = false)
     @Getter
@@ -40,5 +40,35 @@ public class User {
 
     public static UserBuilder builder() {
         return new UserBuilder();
+    }
+    public String getName() {
+        return username;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 }
