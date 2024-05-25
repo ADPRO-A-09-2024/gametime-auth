97166e2a0ba633fe4a3e4b5b313fc75786bc6694
Henrysoed <henry.soedibjo@gmail.com>
2024-05-25 20:56:10 +0700

    [REFACTOR] Add corsConfigurer in AuthApplication to fix cors block error

--- src/main/java/id/ac/ui/cs/advprog/gametime/auth/AuthApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/auth/AuthApplication.java
@@ -2,12 +2,25 @@ package id.ac.ui.cs.advprog.gametime.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            };
+        };
     }
 
 }
