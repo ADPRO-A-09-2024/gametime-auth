0fed62df2782221818443a55723a37cffb02f50c
Henrysoed <henry.soedibjo@gmail.com>
2024-05-26 22:17:03 +0700

    chore: update dependencies and configuration for Prometheus monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
